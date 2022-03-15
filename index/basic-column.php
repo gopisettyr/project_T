887af28489cc26e4296ff52a7144e1ff1eedc984
chaitu4027 <34639738+chaitu1432@users.noreply.github.com>
2022-03-15 23:20:51 +0530

    main page

---
+++ index/basic-column.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Basic Column - Grouped</title>
+
+    <link href="../../assets/styles.css" rel="stylesheet" />
+
+    <style>
+      
+        #chart {
+      //max-width: 200px
+    }
+      
+    </style>
+
+    <script>
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/eligrey-classlist-js-polyfill@1.2.20171210/classList.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn"><\/script>'
+        )
+    </script>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    
+
+    <script>
+      // Replace Math.random() with a pseudo-random number generator to get reproducible results in e2e tests
+      // Based on https://gist.github.com/blixt/f17b47c62508be59987b
+      var _seed = 42;
+      Math.random = function() {
+        _seed = _seed * 16807 % 2147483647;
+        return (_seed - 1) / 2147483646;
+      };
+    </script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+
+    
+  </head>
+
+  <body>
+     <div class="card">
+		<div class="card-header">
+			NIFTY CHANGE
+		</div>
+		<div class="card-body" id="chart2">
+		</div>
+	 </div>
+
+    <script>
+      $(document).ready(function(){
+	  $.getJSON("options_index_json.php", function(res){
+	  //alert(res['PE']);
+        var options = {
+          series: [{
+          name: 'PE',
+          data: res['PE']
+        }, {
+          name: 'CE',
+          data: res['CE']
+        }],
+          chart: {
+          type: 'bar',
+          height: 250
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '50%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['PE Chg','CE Chg'],
+        },
+        yaxis: {
+          title: {
+            text: ''
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val
+            }
+          }
+        }
+        };
+
+        var chart2 = new ApexCharts(document.querySelector("#chart2"), options);
+        chart2.render();
+		})
+      })
+      
+    </script>
+
+    
+  </body>
+</html>
--- index/index.php
+++ index/index.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>frameset attribute</title>
-    </head>
-     
-    <!-- frameset attribute starts here -->   
-    <frameset cols = "50%,50%">
-        <frame name = "frame1" src = "http://localhost/simplexls-master/examples/Project_T/project_T/grouped-bar.html" />
-        <frame name = "frame2" src = "http://localhost/simplexls-master/examples/Project_T/project_T/grouped-bar.html" />
-        <noframes>
-            <body>The browser you are working does not
-                                  support frames.</body>
-        </noframes>
-    </frameset>
-    <!-- frameset attribute ends here -->
+<head>
+	<title>frameset attribute</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="card text-center">
+<div class="card-body">
+<div class="row">
+<div class="col-md-4"> 
+<?php
+include 'grouped-bar.php';
+?>
+</div>
+<div class="col-md-3"> 
+<?php
+include 'basic-column.php';
+include 'banknifty-column.php';
+?>
+</div>
+<div class="col-md-5">
+<?php
+include 'grouped-bar-nifty.php';
+?>
+</div>
+</div>
+<div>
+
+</div>
+</div>
+</div>
+</body>	
 </html>      
