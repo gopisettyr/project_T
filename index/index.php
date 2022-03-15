96ddf8d95efa7e174946258e0297eb59210dd703
chaitu4027 <34639738+chaitu1432@users.noreply.github.com>
2022-03-15 14:49:18 +0530

    index
    index page added

---
+++ index/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>frameset attribute</title>
+    </head>
+     
+    <!-- frameset attribute starts here -->   
+    <frameset cols = "50%,50%">
+        <frame name = "frame1" src = "http://localhost/simplexls-master/examples/Project_T/project_T/grouped-bar.html" />
+        <frame name = "frame2" src = "http://localhost/simplexls-master/examples/Project_T/project_T/grouped-bar.html" />
+        <noframes>
+            <body>The browser you are working does not
+                                  support frames.</body>
+        </noframes>
+    </frameset>
+    <!-- frameset attribute ends here -->
+</html>      
