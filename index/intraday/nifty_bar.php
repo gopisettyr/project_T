74dffdd8acdd6f0a4fcac1de01975bfbf9f49e8c
gopisettyr <ravirockzzteja@gmail.com>
2022-05-02 01:43:11 -0400

    Added Intraday

---
+++ index/intraday/nifty_bar.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Grouped Bar Chart</title>
+
+    <link href="../../assets/styles.css" rel="stylesheet" />
+
+    <style>
+      
+        #chart {
+      max-width: 400px;
+    }
+      
+    </style>
+
+    <script>
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/eligrey-classlist-js-polyfill@1.2.20171210/classList.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn"><\/script>'
+        )
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    
+
+    <script>
+      // Replace Math.random() with a pseudo-random number generator to get reproducible results in e2e tests
+      // Based on https://gist.github.com/blixt/f17b47c62508be59987b
+      var _seed = 42;
+      Math.random = function() {
+        _seed = _seed * 16807 % 2147483647;
+        return (_seed - 1) / 2147483646;
+      };
+    </script>
+
+    
+  </head>
+
+  <body>
+     <div class="card">
+		<div class="card-header">
+			NIFTY OI Compass
+		</div>
+		<div class="card-body" id="chart">
+		</div>
+	 </div>
+
+    <script>
+      $(document).ready(function(){
+	  $.getJSON("nifty_data.php", function(data){
+			var options = {
+			  series: [{
+			  name: 'CE',
+			  data: data.CE
+			}, {
+			  name: 'PE',
+			  data: data.PE
+			}],
+			  chart: {
+			  type: 'bar',
+			  height: 600
+			},
+			plotOptions: {
+			  bar: {
+				horizontal: true,
+				dataLabels: {
+				  position: 'top',
+				},
+			  }
+			},
+			dataLabels: {
+			  enabled: true,
+			  offsetX: 30,
+			  style: {
+				fontSize: '10px',
+				colors: ['grey']
+			  }
+			},
+			stroke: {
+			  show: true,
+			  width: 1,
+			  colors: ['#fff']
+			},
+			tooltip: {
+			  shared: true,
+			  intersect: false
+			},
+                        annotations: {
+    yaxis: [
+      {
+        y: data.ATM[0],
+        borderColor: "black",
+        label: {
+          borderColor: "#00E396",
+          style: {
+            color: "#fff",
+            background: "#00E396"
+          },
+          text: "ATM"
+        }
+      }
+    ],
+  },
+			xaxis: {
+			  categories: data.categories,
+			},
+			};
+			var chart = new ApexCharts(document.querySelector("#chart"), options);
+			chart.render();
+		})
+                setTimeout(function(){
+			$(".apexcharts-gridline").remove();
+		},2000)
+      })
+    </script>
+
+    
+  </body>
+</html>
---
+++ index/intraday/nifty_column_data.php
@@ -0,0 +1,60 @@
+<?php 
+
+//reading json
+$json = file_get_contents('nifty.json');
+$json=str_replace(' ', '', $json);
+$json=str_replace("'", "\"", $json);
+//$json1 = utf8_encode($json);
+$obj = json_decode($json);
+$data=$obj->records->data;
+$i=0;
+$passedvalue=$obj->records->expiryDates[0];
+$pemapping=array();
+$cemapping=array();
+$i=0;
+foreach ($data as $value) {
+	if($value->expiryDate==$passedvalue){
+	   $pemapping[$i]=$value->PE->changeinOpenInterest;
+	   $cemapping[$i]=$value->CE->changeinOpenInterest;
+	   // echo " value is ".$value->CE->changeinOpenInterest."-strike price ".$value->strikePrice;
+	   // echo "<br>";
+	}	
+	$i++;
+}
+
+$petotal=array_sum($pemapping);
+$cetotal=array_sum($cemapping);
+$jsonarray=array("PE"=>array($petotal,0),"CE"=>array(0,$cetotal));
+
+// echo "<br>";
+// echo $petotal;
+// echo "<br>";
+// echo $cetotal;
+echo json_encode($jsonarray);
+/* //forming required inputs array
+$required=array('NIFTY 50','NIFTY BANK','NIFTY IT','NIFTY AUTO','NIFTY MEDIA','NIFTY METAL','NIFTY FIN SERVICE','NIFTY PVT BANK',);
+
+$categoryjson=array();
+$seriesjson=array();
+$mapping=array();
+
+//mapping values in associative array map
+foreach ($data as $value) {
+	if(in_array($value->name, $required)){
+	  $mapping[$value->name]=$value->pChange;
+	}
+}
+
+//sorting in descending order of values 
+arsort($mapping);
+
+//creating two arrays for categories and series
+foreach ($mapping as $key=>$value) {
+	$categoryjson[]=$key;
+	$seriesjson[]=$value;
+}
+
+//forming json with x-axis and y-axis values
+$chartjson=array($categoryjson,$seriesjson);
+echo json_encode($chartjson); */
+?>
